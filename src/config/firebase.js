94738bb36442864fbb44253a0ba30d303c37944a
Zulfikar Hasan <92003902+zullf@users.noreply.github.com>
2026-05-01 08:53:08 +0700

    Replace Firebase config values with placeholders
    Updated Firebase configuration with placeholder values.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7Nh5LCPoo9MG2CUcF-Tq1GXzFCDjBBc",
-  authDomain: "auth-praktikum-50e2a.firebaseapp.com",
-  projectId: "auth-praktikum-50e2a",
-  storageBucket: "auth-praktikum-50e2a.firebasestorage.app",
-  messagingSenderId: "770323972977",
-  appId: "1:770323972977:web:31ada505828c6914ef6cd0",
+  apiKey: "My_Key",
+  authDomain: "My_key",
+  projectId: "My_key",
+  storageBucket: "My_key",
+  messagingSenderId: "My_key",
+  appId: "My_key",
 };
 
 const app = initializeApp(firebaseConfig);
